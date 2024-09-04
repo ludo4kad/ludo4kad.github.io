8204d6a33bef036842adb25e2a67d34b7aad2e2b
ludo4kad <testingggogle@gmail.com>
2024-09-04 21:15:04 +0300

    Add files via upload

--- loading/tg.js
+++ loading/tg.js
@@ -1,3 +1,1 @@
 const tg = window.Telegram.WebApp;
-
-alert('username is ' + tg.initDataUnsafe.user.username);
--- sport-main/crypto/script.js
+++ sport-main/crypto/script.js
@@ -1,1 +1,23 @@
+coinSportValue = 0 || localStorage.getItem("cnspt_val");
+//TG
 const tg = window.Telegram.WebApp;
+const tgUser = tg.initDataUnsafe.user;
+tg.expand();
+//QUERY
+kilim = document.querySelector("li#walletList");
+fejfa = document.querySelector("bottom");
+cnspt = document.querySelector("p.val");
+buttonTap = document.querySelector("button.button");
+
+kilim.textContent = "CNSPT Wallet @" + tgUser.username;
+fejfa.innerHTML = "Hello, " + tgUser.first_name + ". Have a good day!";
+
+function cnsptAdd() {
+    coinSportValue++;
+    cnspt.textContent = coinSportValue+" $CNSPT";
+    localStorage.setItem("cnspt_val", coinSportValue)
+}
+
+buttonTap.addEventListener('click', function() {
+    removeWallet(index);
+});
